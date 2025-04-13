2f1e1813b9482d64f559de7395598187b7b750dc
Documenter.jl <documenter@juliadocs.github.io>
2025-04-13 20:06:23 +0000

    build based on b3316e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SelectedInversion","category":"page"},{"location":"#SelectedInversion","page":"Home","title":"SelectedInversion","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SelectedInversion.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SelectedInversion]","category":"page"}]
+}
